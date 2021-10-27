ce02008b6ce056a99f4f12d0c22352c91751c2ab
Nonich <manip2012@mail.ru>
2021-10-27 17:22:39 +0300

    Fix tests

--- Students.BLL.Tests/Services/CourseApplicationServiceTests.cs
+++ Students.BLL.Tests/Services/CourseApplicationServiceTests.cs
@@ -11,6 +11,8 @@ using Xunit;
 using Students.DAL.Enum;
 using Students.DAL.Specifications;
 using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Identity;
 
 namespace Students.BLL.Tests.Services
 {
@@ -30,69 +32,163 @@ namespace Students.BLL.Tests.Services
             UnitOfWork = new UnitOfWork(new Context(options));
             _mockLogger = new Mock<ILogger<CourseApplication>>();
             _courseApplicationService = new CourseApplicationService(UnitOfWork, _mockLogger.Object);
+  
+        }
+
+   
+        private async Task SetUp()
+        {
+            Random rand = new();
+            List <Course> courses = new();
+            List<Manager> managers = new();
+            List<Teacher> teachers = new();
+            List<Group> groups = new(); 
+            List<Student> students = new();
+            List <CourseApplication> courseApplications = new();
+            for (int i = 1; i < 6; i++)
+            {
+                courses.Add(Fixture.Build<Course>().With(c => c.Id, i).Create()); 
+            }
+            var UserManagers = Fixture.Build<ApplicationUser>().CreateMany<ApplicationUser>(5);
+            for (int i = 1; i < UserManagers.Count(); i++)
+            {
+                await UnitOfWork.ApplicationUsersRepository.AddAsync(UserManagers.ToList()[i]);
+                managers.Add(Fixture.Build<Manager>().With(m => m.Id,i).With(m => m.UserId,UserManagers.ToList()[i].Id).Create());
+ 
+            }
+            var UserTeachers = Fixture.Build<ApplicationUser>().CreateMany<ApplicationUser>(5);
+            for (int i = 1; i < UserTeachers.Count(); i++)
+            {
+                await UnitOfWork.ApplicationUsersRepository.AddAsync(UserTeachers.ToList()[i]);
+                teachers.Add(Fixture.Build<Teacher>().With(m => m.Id, i).With(m => m.UserId, UserTeachers.ToList()[i].Id).Create());
+
+            }
+           
+            for (int i = 1; i < 5; i++)
+            {
+                groups.Add(Fixture.Build<Group>().
+                With(g => g.Id,i).
+                With(g => g.CourseId, courses[i].Id).
+                With(g => g.TeacherId, teachers[i].Id).
+                With(g => g.ManagerId, managers[i].Id).
+                With(g => g.GroupStatus, GroupStatus.Set).
+                With(g => g.CountMax, 30).Create());
+            }
+
+            var UserStudent = Fixture.Build<ApplicationUser>().CreateMany<ApplicationUser>(50);
+            for (int i = 1; i < UserStudent.Count(); i++)
+            {
+                await UnitOfWork.ApplicationUsersRepository.AddAsync(UserStudent.ToList()[i]);
+                students.Add(Fixture.Build<Student>().With(s => s.Id, i).With(s => s.UserId, UserStudent.ToList()[i].Id).Without(s => s.GroupId).Create());
+
+            }
+ 
+            foreach(var item in students)
+            {
+                courseApplications.Add(Fixture.Build<CourseApplication>().Without(c => c.Id).With(c => c.StudentId, item.Id).With(c =>c.CourseId, rand.Next(1,5)).Create());
+            }
+
+            await UnitOfWork.CourseRepository.AddRangeAsync(courses);
+            await UnitOfWork.ManagerRepository.AddRangeAsync(managers);
+            await UnitOfWork.TeacherRepository.AddRangeAsync(teachers);
+            await UnitOfWork.GroupRepository.AddRangeAsync(groups);
+            await UnitOfWork.StudentRepository.AddRangeAsync(students);
+            await UnitOfWork.CourseApplicationRepository.AddRangeAsync(courseApplications);
+            await UnitOfWork.SaveAsync();
         }
 
         [Fact]
-        public async Task Enroll_courseApplication_GetNull()
+        public async Task Enroll_courseApplicationById_Null()
         {
             //Arrange
-            var courseApplications = Fixture.Build<CourseApplication>().Without(c => c.Id).CreateMany<CourseApplication>(20);
-            var students = Fixture.CreateMany<Student>(20);
-            await UnitOfWork.CourseApplicationRepository.AddRangeAsync(courseApplications);
-            await UnitOfWork.SaveAsync();
-
             //Act 
-            var result = await _courseApplicationService.GetAsync(0);
-
             //Assert 
             await Assert.ThrowsAsync<InvalidOperationException>(() => _courseApplicationService.Enroll(0));
         }
 
         [Fact]
-        public async Task Enroll_student_GetNotNullIdGroup()
+        public async Task Enroll_studentByGroupId_Null()
         {
             //Arrange
             var group = Fixture.Build<Group>().With(g => g.Id, 1).Create();
-            var student = Fixture.Build<Student>().With(s => s.Id,1).With(s => s.GroupId, group.Id).Create();
-            var courseApplication = Fixture.Build<CourseApplication>().With(c => c.Id,1).With(c => c.StudentId, student.Id).Create();
+            var student = Fixture.Build<Student>().With(s => s.Id, 1).With(s => s.GroupId, group.Id).Create();
+            var courseApplication = Fixture.Build<CourseApplication>().With(c => c.Id, 1).With(c => c.StudentId, student.Id).Create();
             await UnitOfWork.GroupRepository.AddAsync(group);
             await UnitOfWork.StudentRepository.AddAsync(student);
             await UnitOfWork.CourseApplicationRepository.AddAsync(courseApplication);
             await UnitOfWork.SaveAsync();
-            var specStudent = new StudentWithItemsSpecifications(courseApplication.StudentId);
-   
-            //Act 
-            var result = await UnitOfWork.StudentRepository.GetAsync(specStudent, false);
-
+            //Act   
             //Assert 
-            Assert.NotNull(result.GroupId);
+            await Assert.ThrowsAsync<InvalidOperationException>(() => _courseApplicationService.Enroll(courseApplication.Id));
         }
 
         [Fact]
-        public async Task Enroll_student_PresenceGroupId()
+        public async Task Enroll_Group_Null()
         {
             //Arrange
-            int courseApplicationId = 1;
-            int studentId = 1;
-            int groupId = 1;
-            var courseId = 1;
-            var group = new Group() {Id = groupId, GroupStatus = GroupStatus.Set, CourseId = courseId,CountMax = 15 };
-            var student = new Student() { Id = studentId };
-            var courseApplication = new CourseApplication() { Id = courseApplicationId, StudentId = student.Id, CourseId = courseId };
-          // var student = Fixture.Build<Student>().With(s => s.Id, studentId).Without(s => s.GroupId).Create();
-          // var courseApplication = Fixture.Build<CourseApplication>().With(c => c.StudentId, student.Id).With(c => c.Id, courseApplicationId).Create();
+            var group = Fixture.Build<Group>().With(g => g.Id, 1).Create();
+            var student = Fixture.Build<Student>().With(s => s.Id, 1).Without(s => s.GroupId).Create();
+            var courseApplication = Fixture.Build<CourseApplication>().With(c => c.Id, 1)
+                .With(c => c.StudentId, student.Id).Create();
+            var groups = Fixture.Build<Group>().With(g => g.CourseId, courseApplication.CourseId)
+                .With(g => g.CountMax, 30)
+                .With(g => g.GroupStatus, GroupStatus.Training).CreateMany(3);
             await UnitOfWork.GroupRepository.AddAsync(group);
             await UnitOfWork.StudentRepository.AddAsync(student);
             await UnitOfWork.CourseApplicationRepository.AddAsync(courseApplication);
+            await UnitOfWork.GroupRepository.AddRangeAsync(groups);
             await UnitOfWork.SaveAsync();
+            //Act   
+            //Assert 
+            await Assert.ThrowsAsync<InvalidOperationException>(() => _courseApplicationService.Enroll(courseApplication.Id));
+        }
 
+        [Fact]
+        public async Task Enroll_GroupByCountMax_Null()
+        {
+            //Arrange
+            var group = Fixture.Build<Group>().With(g => g.Id, 1).Create();
+            var student = Fixture.Build<Student>().With(s => s.Id, 1).Without(s => s.GroupId).Create();
+            var courseApplication = Fixture.Build<CourseApplication>().With(c => c.Id, 1)
+                .With(c => c.StudentId, student.Id).Create();
+            var groups = Fixture.Build<Group>().With(g => g.CourseId, courseApplication.CourseId)
+                .With(g => g.CountMax, 0)
+                .With(g => g.GroupStatus, GroupStatus.Set).CreateMany(3);
+            await UnitOfWork.GroupRepository.AddAsync(group);
+            await UnitOfWork.StudentRepository.AddAsync(student);
+            await UnitOfWork.CourseApplicationRepository.AddAsync(courseApplication);
+            await UnitOfWork.GroupRepository.AddRangeAsync(groups);
+            await UnitOfWork.SaveAsync();
+            //Act   
+            //Assert 
+            await Assert.ThrowsAsync<InvalidOperationException>(() => _courseApplicationService.Enroll(courseApplication.Id));
+        }
 
-            //Act 
-            await _courseApplicationService.Enroll(courseApplicationId);
-            var result = await UnitOfWork.StudentRepository.GetByIdAsync(studentId);
+        [Fact]
+        public async Task Enroll_Success_CourseStatusClose()
+        {
+            //Arrange
+            var group = Fixture.Build<Group>().With(g => g.Id, 1).Create();
+            var student = Fixture.Build<Student>().Without(s => s.GroupId)
+                .Without(s => s.Group).With(s => s.Id, 1).Create();
+            var courseApplication = Fixture.Build<CourseApplication>().With(c => c.Id, 1)
+                .With(c => c.StudentId, student.Id).Create();
+            var groups = Fixture.Build<Group>().With(g => g.CourseId, courseApplication.CourseId)
+                .With(g => g.CountMax, 15)
+                .With(g => g.GroupStatus, GroupStatus.Set).CreateMany(3);
+            await UnitOfWork.GroupRepository.AddAsync(group);
+            await UnitOfWork.StudentRepository.AddAsync(student);
+            await UnitOfWork.CourseApplicationRepository.AddAsync(courseApplication);
+            await UnitOfWork.GroupRepository.AddRangeAsync(groups);
+            await UnitOfWork.SaveAsync();
+            await _courseApplicationService.Enroll(courseApplication.Id);
+
+            //Act   
+            var spec = new CourseApplicationWithItemsSpecifications(student.Id);
+            var result = await UnitOfWork.CourseApplicationRepository.GetAsync(spec, false);
 
             //Assert 
-            Assert.Equal(groupId,result.GroupId);
+            Assert.Equal(ApplicationStatus.Close,result.ApplicationStatus);
         }
 
     }
